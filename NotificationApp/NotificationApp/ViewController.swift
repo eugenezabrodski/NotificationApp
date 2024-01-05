56faf446ffb8243f5a184abbcce94e4cc7a2ef38
Eugene <115879896+eugenezabrodski@users.noreply.github.com>
2024-01-05 11:45:56 +0300

    create

---
+++ NotificationApp/NotificationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotificationApp
+//
+//  Created by Eugene on 05/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
